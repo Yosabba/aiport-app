781251c1004aad0d3e5edae2040fe6b7ea8dde80
Dominic Johnson <85464248+Yosabba@users.noreply.github.com>
2022-02-16 20:40:21 -0500

    :zap: Landing page background video & updates

--- src/App.js
+++ src/App.js
@@ -1,31 +1,48 @@
 import { Box, Heading, SimpleGrid, Flex } from "@chakra-ui/react";
 import GetUserLocation from "./Components/GetUserLocations";
-import Videonoob from './assets/waiting.mp4';
+import Videonoob from "./assets/waiting.mp4";
 
 const App = () => {
-  //https://photos.google.com/search/_tra_/photo/AF1QipPFsO_8rmoLmp5vbPSI9-L07np1JbNRcu_0VAcO
   return (
-    <Flex direction="column" align="center" justify="center" >
-      <video autoPlay loop muted
-      style={{
-        width: "100vw",
-        height: "100vh",
-        objectFit: "cover",
-        objectPosition: "center",
-        position: "absolute",
-        top: 0,
-        left: 0,
-        zIndex: -1
-      }}>
-        <source
-          src={Videonoob}
-          type="video/mp4"
-        />
-      </video>
-      <Heading as="h1" my="4rem">
-        Welcome to Airportly
-      </Heading>
-      <GetUserLocation />
+    <Flex direction="column" align="center" justify="center">
+      <Box
+        sx={
+          {
+            // position: "fixed",
+            // display: "none",
+            // width: "100%",
+            // height: "100%",
+            // top: "0",
+            // left: "0",
+            // right: "0",
+            // bottom: "0",
+            // backgroundColor: "rgba(0,0,0,0.5)",
+            // zIndex: "-2",
+          }
+        }
+      >
+        <video
+          autoPlay
+          loop
+          muted
+          style={{
+            width: "100vw",
+            height: "100vh",
+            objectFit: "cover",
+            objectPosition: "center",
+            position: "absolute",
+            top: 0,
+            left: 0,
+            zIndex: -1,
+          }}
+        >
+          <source src={Videonoob} type="video/mp4" />
+        </video>
+        <Heading as="h1" my="4rem">
+          Welcome to Airportly
+        </Heading>
+        <GetUserLocation />
+      </Box>
     </Flex>
   );
 };
